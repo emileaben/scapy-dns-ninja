aef7111e6f8fc99f8bf8b37b7848191ebb87534e
Emile Aben <emileaben@gmail.com>
2014-11-24 14:37:07 +0100

    working version 2011-11-24 (problems turned out to be ICMP messages)

--- dns-ninja-server.py
+++ dns-ninja-server.py
@@ -71,61 +71,62 @@ def DNS_Responder(localIP):
         global dest_idx
         if (DNS in pkt and pkt[DNS].opcode == 0L and pkt[DNS].ancount == 0 and pkt[IP].src != localIP):
             print pkt[DNS].qd
-            if ( pkt[DNS].qd.qtype == 1 ): ###  1 = 'A'
-                dest_ip = conf['ServerIP']
-                #if pkt[DNS].qd.qname.lower() != conf['ServerName']:
-                if not conf['ServerName'] in pkt['DNS Question Record'].qname:
-                    dest_ip = dests[dest_idx]
-                    dest_idx += 1
-                    if dest_idx >= dests_len: dest_idx = 0
-                else:
-                    print "lower: %s" % ( pkt[DNS].qd.qname.lower() )
-                try:
-                    resp = generate_A_response( pkt, dest_ip )
-                    send(resp,verbose=0)
-                    return "sent resp for %s" % ( dest_ip )
-                except:
-                     print "error on packet: %s" % ( pkt.summary() )
-                     print sys.exc_info()
-            elif ( pkt[DNS].qd.qtype == 2 ): ###  2 = 'NS'
-                # regardless of the question we only know one answer
-                print "we got an NS request, exiting!"
-                resp = IP(dst=pkt[IP].src, id=pkt[IP].id)\
-                       /UDP(dport=pkt[UDP].sport, sport=53)\
-                       /DNS( id=pkt[DNS].id,
-                                  aa=1, #we are authoritative
-                                  qr=1, #it's a response
-                                  rd=pkt[DNS].rd, # copy recursion-desired
-                                  qdcount=pkt[DNS].qdcount, # copy question-count
-                                  qd=pkt[DNS].qd, # copy question itself
-                                  ancount=1, #we provide a single answer
-                                  an=DNSRR(rrname=conf['ServerDomain'], ttl=86400, type='NS', rdata=conf['ServerName'] )
-                       )
-                send(resp,verbose=0)
-            elif ( pkt[DNS].qd.qtype == 6 ): ###  6 = 'SOA'
-                print "we got a SOA request, exiting!"
-                soa = DNSSOARecord( mname=conf['ServerName'], rname="root.%s" % ( conf['ServerName'] ) ) 
-                resp = IP(dst=pkt[IP].src, id=pkt[IP].id)\
-                       /UDP(dport=pkt[UDP].sport, sport=53)\
-                       /DNS( id=pkt[DNS].id,
-                                  aa=1, #we are authoritative
-                                  qr=1, #it's a response
-                                  rd=pkt[DNS].rd, # copy recursion-desired
-                                  qdcount=pkt[DNS].qdcount, # copy question-count
-                                  qd=pkt[DNS].qd, # copy question itself
-                                  ancount=1, #we provide a single answer
-                                  an=DNSRR(
-                                       rrname=conf['ServerDomain'],
-                                       ttl=86400,
-                                       type=6,
-                                       rdata=str(soa)
-                                  )
-                       )
-                send(resp,verbose=0)
-            else:
-                print "we got a not A/NS/SOA request, exiting!"
-        else:
-            return pkt.summary()
+            try:
+                    if ( pkt[DNS].qd.qtype == 1 ): ###  1 = 'A'
+                        dest_ip = conf['ServerIP']
+                        #if pkt[DNS].qd.qname.lower() != conf['ServerName']:
+                        if not conf['ServerName'] in pkt['DNS Question Record'].qname:
+                            dest_ip = dests[dest_idx]
+                            dest_idx += 1
+                            if dest_idx >= dests_len: dest_idx = 0
+                        else:
+                            print "lower: %s" % ( pkt[DNS].qd.qname.lower() )
+                        try:
+                            resp = generate_A_response( pkt, dest_ip )
+                            send(resp,verbose=0)
+                            return "sent resp for %s" % ( dest_ip )
+                        except:
+                             print "error on packet: %s" % ( pkt.summary() )
+                             print sys.exc_info()
+                    elif ( pkt[DNS].qd.qtype == 2 ): ###  2 = 'NS'
+                        # regardless of the question we only know one answer
+                        print "we got an NS request, exiting!"
+                        resp = IP(dst=pkt[IP].src, id=pkt[IP].id)\
+                               /UDP(dport=pkt[UDP].sport, sport=53)\
+                               /DNS( id=pkt[DNS].id,
+                                          aa=1, #we are authoritative
+                                          qr=1, #it's a response
+                                          rd=pkt[DNS].rd, # copy recursion-desired
+                                          qdcount=pkt[DNS].qdcount, # copy question-count
+                                          qd=pkt[DNS].qd, # copy question itself
+                                          ancount=1, #we provide a single answer
+                                          an=DNSRR(rrname=conf['ServerDomain'], ttl=86400, type='NS', rdata=conf['ServerName'] )
+                               )
+                        send(resp,verbose=0)
+                    elif ( pkt[DNS].qd.qtype == 6 ): ###  6 = 'SOA'
+                        print "we got a SOA request, exiting!"
+                        soa = DNSSOARecord( mname=conf['ServerName'], rname="root.%s" % ( conf['ServerName'] ) ) 
+                        resp = IP(dst=pkt[IP].src, id=pkt[IP].id)\
+                               /UDP(dport=pkt[UDP].sport, sport=53)\
+                               /DNS( id=pkt[DNS].id,
+                                          aa=1, #we are authoritative
+                                          qr=1, #it's a response
+                                          rd=pkt[DNS].rd, # copy recursion-desired
+                                          qdcount=pkt[DNS].qdcount, # copy question-count
+                                          qd=pkt[DNS].qd, # copy question itself
+                                          ancount=1, #we provide a single answer
+                                          an=DNSRR(
+                                               rrname=conf['ServerDomain'],
+                                               ttl=86400,
+                                               type=6,
+                                               rdata=str(soa)
+                                          )
+                               )
+                        send(resp,verbose=0)
+                    else:
+                        print "we got a not A/NS/SOA request, exiting!"
+            except:
+                print "no qd.qtype in this dns request?!"
     return getResponse
 
 filter = "udp port 53 and ip dst %s and not ip src %s" % (conf['ServerIP'], conf['ServerIP'])

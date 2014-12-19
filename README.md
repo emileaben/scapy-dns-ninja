911665dd15eb1cd67052f636f5d25594e0d878c0
Emile Aben <emileaben@gmail.com>
2014-12-19 05:32:51 +0100

    support for v4 and v6 lists

--- README.md
+++ README.md
@@ -8,15 +8,18 @@ needs a local config file called 'ninja-server.conf' in yaml format. Example:
 
 This code acts as a DNS server, and when it receives an A or AAAA request it returns an IP address from a specified list.
 
-There is a default list (ips.txt) that is read from the same dir as the server is started from, or if it is queried in the form:
+For DNS A queries lists are called *dests.v4.txt*, for AAAA queries *dests.v6.txt*.
+
+If the server is queried for a label in this form:
 
     <listname>.<serverdomain>
 
-it will try to find an ips.txt file in the local <listname> directory, and serve IPs from there.
+it will try to find a dests.v[46].txt file in the local <listname> directory, and serve IPs from there.
+If there is no match it will use the IP-lists file(s) in the `_default` directory.
 
 example (with config file above):
 
 query for amsterdam-nl.random-ip.emilaben.com
 
-will cause the server to look for an ip-list in the local ./amsterdam-nl/ directory. If it exists it will load it, randomize the list, and serve an IP from it. The next query for this name will get the next IP from the randomized list.
+will cause the server to look for an ip-list file (either dests.v[46].txt) in the local ./amsterdam-nl/ directory. If it exists it will load it, randomize the list, and serve an IP from it. The next query for this name will get the next IP from the randomized list.
 At the end of the list, it will be randomized again, and the first IP from the randomized list will be returned again.
--- dns-ninja-server.py
+++ dns-ninja-server.py
@@ -14,31 +14,46 @@ def read_conffile( filename ):
       with open(filename,'r') as fh:
          conf = yaml.load( fh )
    except:
-      print "Error reading config file: %s" % ( filename )
+      print >>sys.stderr,"Error reading config file: %s" % ( filename )
    return conf
 
 conf = read_conffile('ninja-server.conf')
-lists = {}
+lists = {'v4':{},'v6':{}}
 
-def read_destfile( list_name, lists ):
-    filename = "./%s/ips.txt" % ( list_name )
+def read_destfile( list_name, lists, proto ):
+    filename = "./%s/dests.%s.txt" % ( list_name, proto )
+    print >>sys.stderr, "trying to read list %s / proto %s from file: %s" % ( list_name, proto, filename )
     dests = []
     with open(filename,'r') as fh:
         for line in fh:
             line = line.rstrip('\n')
             dests.append( line ) 
     shuffle( dests )
-    lists[ list_name ] = { 
+    if not proto in lists:
+        lists[ proto ] = {}
+    lists[ proto][ list_name ] = { 
         'dests': dests,
         'mtime': os.path.getmtime( filename ),
         'length': len(dests),
         'dest_idx': 0
     }
-    print >>sys.stderr, "list %s successfully loaded" % ( list_name )
+    print >>sys.stderr, "list %s / proto %s successfully loaded" % ( list_name, proto )
     return lists
 
 ## read the default list
-read_destfile('_default',lists)
+lists_read=0
+try:
+   read_destfile('_default',lists,'v4')
+   lists_read += 1
+except: pass
+try:
+   read_destfile('_default',lists,'v6')
+   lists_read += 1
+except: pass
+### need at least v4/v6 list
+if not lists_read > 0:
+   print >>sys.stderr,"need at least a default IPv4 or default IPv6 list"
+   sys.exit(0)
 
 def generate_A_response( pkt, dest_ip ):
    resp = IP(dst=pkt[IP].src, id=pkt[IP].id)\
@@ -54,46 +69,58 @@ def generate_A_response( pkt, dest_ip ):
       )
    return resp
 
+def record( src, list_name, proto, dest_ip ):
+   ''' write we sent this pkt somewhere '''
+   print "src=%s list=%s proto=%s dest=%s" % ( src, list_name, proto, dest_ip )
+
 def DNS_Responder(conf,lists):
     #TODO better regex
     re_getlist = re.compile(r'([a-z0-9\-]+)\.%s\.$' % ( conf['ServerDomain'] ) )
     def getResponse(pkt):
-        print "RECEIVED: %s" % ( pkt.summary() )
+        print >>sys.stderr,"RECEIVED: %s" % ( pkt.summary() )
         global dest_idx
         if (DNS in pkt and pkt[DNS].opcode == 0L and pkt[DNS].ancount == 0 and pkt[IP].src != conf['ServerIP']):
             try:
-               if ( pkt[DNS].qd.qtype in [1,28] ): ###  A or AAAA
-                  # sensible default
-                  list_name = '_default'
-                  list_match = re.search( re_getlist, pkt[DNS].qd.qname.lower() )
-                  if list_match and os.path.exists( "./%s/ips.txt" % ( list_match.group(1) ) ):
-                      list_name = list_match.group(1)
-                      if list_name not in lists or os.path.getmtime("./%s/ips.txt" % (list_name) ) > lists[list_name]['mtime']:
-                         ## read if the list wasn't read yet or if the mtime changed
-                         read_destfile( list_name, lists )
-                  try:
-                      dest_idx = lists[list_name]['dest_idx']
-                      dest_ip = lists[list_name]['dests'][dest_idx]
-                      if lists[list_name]['dest_idx'] < lists[list_name]['length']-1:
-                          lists[list_name]['dest_idx'] += 1
-                      else:
-                          print "list reset %s" % ( list_name )
-                          #TODO shuffle? configurable?
-                          shuffle( lists[list_name]['dests'] )
-                          lists[list_name]['dest_idx'] = 0 ## reset to beginning
-                      resp = generate_A_response( pkt, dest_ip )
-                      send(resp,verbose=0)
-                      return "sent resp for %s" % ( dest_ip )
-                  except:
-                       print "error on packet: %s" % ( pkt.summary() )
-                       print sys.exc_info()
+               pkt_proto = None
+               if pkt[DNS].qd.qtype == 1:
+                  pkt_proto='v4'  
+               elif pkt[DNS].qd.qtype == 28:
+                  pkt_proto='v6'  
+               else: ### won't respond to non A or AAAA packet
+                  return
+               # sensible default
+               list_name = '_default'
+               list_match = re.search( re_getlist, pkt[DNS].qd.qname.lower() )
+               if list_match and os.path.exists( "./%s/dests.%s.txt" % ( list_match.group(1), pkt_proto ) ):
+                   list_name = list_match.group(1)
+                   if list_name not in lists[pkt_proto] or os.path.getmtime("./%s/dests.%s.txt" % (list_name, pkt_proto) ) > lists[pkt_proto][list_name]['mtime']:
+                      ## read if the list wasn't read yet or if the mtime changed
+                      read_destfile( list_name, lists )
+               try:
+                   dest_idx = lists[pkt_proto][list_name]['dest_idx']
+                   dest_ip = lists[pkt_proto][list_name]['dests'][dest_idx]
+                   if lists[pkt_proto][list_name]['dest_idx'] < lists[pkt_proto][list_name]['length']-1:
+                       lists[pkt_proto][list_name]['dest_idx'] += 1
+                   else:
+                       print "list reset %s/%s" % ( pkt_proto,list_name )
+                       #TODO shuffle? configurable?
+                       shuffle( lists[pkt_proto][list_name]['dests'] )
+                       lists[pkt_proto][list_name]['dest_idx'] = 0 ## reset to beginning
+                   resp = generate_A_response( pkt, dest_ip )
+                   send(resp,verbose=0)
+                   record( pkt[IP].src, list_name, pkt_proto, dest_ip )
+                   return "sent resp for %s" % ( dest_ip )
+               except:
+                    print >>sys.stderr,"error on packet: %s" % ( pkt.summary() )
+                    print >>sys.stderr,sys.exc_info()
                else:
                   print "query type not supported"
             except:
-                print "%s" % ( traceback.print_tb( sys.exc_info()[2] ) )
+                print >>sys.stderr,"%s" % ( traceback.print_tb( sys.exc_info()[2] ) )
         else: 
             print "no qd.qtype in this dns request?!"
     return getResponse
 
+print >>sys.stderr, "config loaded, starting operation"
 filter = "udp port 53 and ip dst %s and not ip src %s" % (conf['ServerIP'], conf['ServerIP'])
 sniff(filter=filter,store=0,prn=DNS_Responder(conf,lists))
